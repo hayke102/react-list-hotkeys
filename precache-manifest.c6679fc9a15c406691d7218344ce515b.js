401af23a8a3f999a33d5f8a8510fc73647523ce0
Yuval Hayke <hayke102@gmail.com>
2021-03-14 23:49:28 +0200

    Updates

---
+++ precache-manifest.c6679fc9a15c406691d7218344ce515b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f6b6968ed97cb175dd8fcb4ba594b93",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d541b8b58b8ec6303de",
+    "url": "./static/css/main.f8b702c2.chunk.css"
+  },
+  {
+    "revision": "5126acbcc40433b9fba1",
+    "url": "./static/js/2.601ddbc7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.601ddbc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d541b8b58b8ec6303de",
+    "url": "./static/js/main.82b29811.chunk.js"
+  },
+  {
+    "revision": "b63a6862f31e0f01862e",
+    "url": "./static/js/runtime-main.ce0b1f8b.js"
+  }
+]);
